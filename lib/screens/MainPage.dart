cf12bb618df8c84612387fd5168c8e544a5951a2
CM-Talha <muhammedtalha81@gmail.com>
2021-01-27 01:17:22 +0500

    continued

--- lib/screens/MainPage.dart
+++ lib/screens/MainPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:glass_container/glass_container.dart';
 
 class MainPage extends StatefulWidget {
   MainPage({Key key}) : super(key: key);
@@ -11,36 +12,59 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-        bottom: TabBar(
-          tabs: [
-            Icon(Icons.home),
-            Icon(Icons.mail),
-            Icon(Icons.bookmark),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.2), BlendMode.lighten),
+            image: AssetImage('lib/BG.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          showSelectedLabels: true,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'a'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'b'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'c'),
           ],
-          onTap: (a) {
-            a += a;
-            print(a);
-          },
         ),
       ),
-      body: SingleChildScrollView(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/BG.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                child:ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 1,
-                  itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    color: Colors.amber,
-                  );
-                 },
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GlassContainer(
+                    contHeight: 200,
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GlassContainer(
+                    contHeight: 200,
+                  ),
+                ),
+                Container(
+                  height: 300,
+                ),
+                Container(
+                  height: 300,
+                ),
+              ],
+            ),
           ),
         ),
       ),
